874e2bae80b68a784873c47656d60c5a4908b0cd
Vojta Vonasek <vonasek@labe.felk.cvut.cz>
2024-02-14 08:46:01 +0100

    run exp vojta

--- runExperiments.py
+++ runExperiments.py
@@ -19,43 +19,49 @@ planners = {}
 #planners["rrt"] = "-planner 2 -d_crit 100000 "  #rrt, no goal bias
 
 #planners["jrbt"] = "-planner 1 " #burs rrt + j+ expand
-#planners["jrrt"] = "-planner 3 -d_crit 100000 " #rrt, alternating random expansion + goal bias 
+planners["jrrt"] = "-planner 3 -d_crit 100000 " #rrt, alternating random expansion + goal bias 
 #planners["ikrbt"] = "-planner 4 "   #goal is IK solution, goes to only single goal
 #planners["ikrrt"] = "-planner 5 -d_crit 100000 "  #goal is IK solution, goes to only single goal
+planners["jrbtNew"] = "-planner 6 -epsilon_q 0.2 -preheat_ratio 0.2 " #burs rrt + j+ expand
 
 fout = open("all-cmds.sh", "wt")
     
-rrtSize = 120 * 1000
-distanceToGoal = 0.05  #should be 0.05!!
+rrtSize = 40 * 1000
+distanceToGoal = 0.07  #should be 0.05!!
 dcrit = 0.11
-goalBiasProbability = 0.7 #goal bias neer the goal, should be larger than goalBiasProbability2
-goalBiasProbability2 = 0.01
+
+goalBiasRadius = 0.2
+goalBiasProbability = 0.8 #goal bias neer the goal, should be larger than goalBiasProbability2
+prob_steer = 0.05 #steer
     
 #urdfFile = "jogramop/robots/franka_panda/mobile_panda.urdf"
 #urdfFile = "jogramop/robots/franka_panda/mobile_panda_fingers.urdf"
 urdfFile = "jogramop/robots/franka_panda/mobile_panda_fingersSmallMesh.urdf"
 seed = 1
-
-for sprob in [0.01, 0.02, 0.05, 0.1, 0.2, 0.3 ]:
+"""
+for sprob in [0.05, 0.1, 0.2, 0.3 ]:
     for gbr in [0.1, 0.2, 0.3, 0.5]:
         for gbprob in [0.1, 0.2, 0.7, 0.9 ]:
             planners["jrbt-steer{}-gbr{}-gprob{}".format(sprob,gbr,gbprob)] = " -planner 1 -goal_bias_radius {} -prob_steer {} -goal_bias_prob {} ".format(gbr,sprob,gbprob) 
+"""
 
-
-for scenario in range(1,5):
+for scenario in range(1,12+1):
     scenarioDir = "jogramop/scenarios/{:03d}/export/".format(scenario)
     obstacleFile = "{}/obstacles.obj".format(scenarioDir)
     startFile = "{}/robot_start_conf.csv".format(scenarioDir)
     ikFile = "{}/grasp_IK_solutions.csv".format(scenarioDir)
     graspFile = "{}/grasps.csv".format(scenarioDir)
 
-    graspConfigurations = loadGrasps(ikFile)
     
     for planner in planners:
         resultsDir = "{}/{}/{}".format(outputDir, scenario, planner)
         os.system("mkdir -p {}".format(resultsDir))
 
-        for iteration in range(100):
+        for iteration in range(80):
+            graspConfigurations = loadGrasps(ikFile)
+
+            if planner == "jrbt":
+                graspConfigurations = [1]; #it's not in fact used for this planner, this just forces that jrbt planner runs only once
 
             for ikindex in range(len(graspConfigurations)):
                 if planner != "ikrrt" and planner != "ikrbt" and ikindex > 0:
@@ -69,18 +75,18 @@ for scenario in range(1,5):
                     print("Result ", outFile, " finished ")
                     continue
 
-                cmd = "timeout 150s ./burs_of_free_space test "
+                cmd = "timeout 30s ./burs_of_free_space test "
                 cmd += " -grasp {} -urdf {} -obstacle {} -start_config {}".format(graspFile, urdfFile, obstacleFile, startFile)
-                cmd += " -delta_q 3.1415 -epsilon_q 0.05 -num_spikes 7  "
+                cmd += " -delta_q 1.3 -epsilon_q 0.2 -num_spikes 7  "
                 cmd += " -render 0 -vis_script scripts/animate_scene.py -cx -1 -cy 3 -cz 6 -groundLevel 0.00 -minColSegIdx 6 "
                 cmd += " -target_prefix {} ".format(outFile)
                 cmd += " -d_crit {} ".format(dcrit)
                 cmd += " -max_iters {} ".format(rrtSize)
                 cmd += " -p_close_enough {} ".format(distanceToGoal)
                 cmd += " -q_resolution 0.1 "
-                cmd += " -goal_bias_radius 0.25 " #bylo 0.5
+                cmd += " -goal_bias_radius {} ".format(goalBiasRadius)
                 cmd += " -goal_bias_prob {} ".format(goalBiasProbability)
-                cmd += " -prob_steer {} ".format(goalBiasProbability2)
+                cmd += " -prob_steer {} ".format(prob_steer)
                 cmd += " -seed {} ".format(seed)
                 cmd += planners[ planner ]  #when some cmdline option is repearing, the last one is accepted, so this line must be last in cmd
                 seed += 1
